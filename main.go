619697766a40b5c323953b8b7a9044bebdb0e47a
aditya9026 <akumar@akaruilabs.com>
2019-10-03 13:15:04 +0530

    minor fix for heroku

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ func main() {
 	router.HandleFunc("/api/blocks/{id:[0-9]+}", controllers.GetBlocksFor).Methods("GET")
 	router.HandleFunc("/api/transactions/{id:[0-9]+}", controllers.GetTransactionsFor).Methods("GET")
 	corsObj := handlers.AllowedOrigins([]string{"*"})
-	log.Fatal(http.ListenAndServe(":3001", handlers.CORS(corsObj)(router)))
 	// router.NotFoundHandler = app.NotFoundHandler
 
 	port := os.Getenv("PORT")
@@ -28,6 +27,7 @@ func main() {
 
 	fmt.Println("port", port)
 
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(corsObj)(router)))
 	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:3000/api
 	if err != nil {
 		fmt.Print(err)
